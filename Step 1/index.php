318fdba69e324a0b81633e4ec9d14f2eff442409
Christopher-Cannon <Christopher-Cannon@users.noreply.github.com>
2017-08-30 14:19:17 +0100

    Added step one

---
+++ Step 1/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html>
+	<head>
+		<title>Database entry</title>
+
+		<meta charset="UTF-8">
+
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+	</head>
+
+	<body>
+		<div class="main-wrapper">
+			<h1>Leave a comment</h1>
+
+			<div class="form-wrapper">
+				<form>
+					<label>Name</label>
+					<input type="text" name="name" />
+
+					<label>Comment</label>
+					<textarea name="comment"></textarea>
+
+					<input type="submit" name="submit" value="Submit" />
+				</form>
+			</div>
+
+			<div class="comment-list">
+				
+			</div>
+		</div>
+	</body>
+</html>
